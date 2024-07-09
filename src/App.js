066f0f955732fc053d8ff1c1b10b5c37a4dcd504
Ankit Pradhan <90372599+Jonapra@users.noreply.github.com>
2024-07-09 10:58:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import Navbar from './components/Header/Header.js';
 import Body from './components/Body/Body.js';
 import { SidebarProvider } from './utils/UseSidebar';
+import { Provider } from'react-redux';
+import store from "./utils/store.js";
 
 const App = () => {
   return (
-    <SidebarProvider>
-      <div className="App">
-        <Navbar />
-        <Body />
-      </div>
-    </SidebarProvider>
+    <Provider store={store}>
+        <SidebarProvider>
+          <div className="App">
+            <Navbar />
+            <Body />
+          </div>
+        </SidebarProvider>
+    </Provider>
   );
 };
 
