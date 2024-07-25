e0e072922ea0556feca12ff80f2726a4993e1294
Ankit Pradhan <90372599+Jonapra@users.noreply.github.com>
2024-07-25 23:33:32 +0530

    Update Chats.js

--- src/components/watchVideo/Livechats/Chats.js
+++ src/components/watchVideo/Livechats/Chats.js
@@ -5,7 +5,7 @@ const Chats = () => {
   return (
     <div className='liveChats-container border-2 border-black w-[401px] h-[595px] rounded-lg '>
         <h2 className='font-bold px-2 py-2 border-b-2 border-black text-lg'>Top Chats</h2>
-        <ChatsMessage name={'Ankit Pradhan'} message={"This is a live chat message"} />
+        <ChatsMessage name={'Ankit Pradhan'} message={"Hello this is a live chat message"} />
     </div>
   )
 }
